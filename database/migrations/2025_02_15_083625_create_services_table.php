cdc499de872550cb2ffc64afb050e200f6675fee
Vigitoreanec <chirik39@mail.ru>
2025-02-15 10:44:12 +0200

    1) модели - php artisan make:model Client -m - php artisan make:model Master -m - php artisan make:model Category -m - php artisan make:model Service -m - php artisan make:model Meeting -m - php artisan make:model Comment -m 2) мастер и услуг (many-to-many) php artisan make:migration create_master_service_table

---
+++ database/migrations/2025_02_15_083625_create_services_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->foreignId('category_id')->constrained();
+            // CONSTRAINT - ключевое слово, которое указывает, что в данной секции описывается ограничение, 
+            // которое налагается на данные в таблице, и которое будет проверяться подсистемой контроля целостности 
+            // и непротиворечивости данных сервера, что в свою очередь не позволит внести или изменить данные в таблице так, 
+            // чтобы это условие не выполнялось, а при выявлении факта наличия не соответствующих этому условию 
+            // данных в таблице она будет считаться повреждённой. 
+            // Сразу после этого ключевого слова указывается уникальный идентификатор (имя) этого ограничения 
+            // (он может быть пропущен, тогда сервер сгенерирует его автоматически).
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
---
+++ database/migrations/2025_02_15_084025_create_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->foreignId('client_id')->constrained();
+            $table->morphs('commentable'); // создает commentable_id и commentable_type
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
